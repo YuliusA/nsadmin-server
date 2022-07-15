8b7c4013a9e468b6d8cd7c666b3681cca40855ab
Yulius <yulius.adrianto@nusatrip.com>
2022-07-15 14:23:26 +0700

    allowedOrigins add

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'https://nsadmin-server.herokuapp.com',
     'https://ns-admin-backend.herokuapp.com',
     'https://reduxjwtauth.netlify.app',
-    'https://nsdmin-react.netlify.app',
+    'https://nsadmin-client.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
